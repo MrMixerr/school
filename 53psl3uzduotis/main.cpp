3941ac69c5d80ead4709443694197c23e4c9480b
MrMixerr <52745857+MrMixerr@users.noreply.github.com>
2023-12-13 13:46:14 +0200

    padaryt nd cia lol :DDDD

---
+++ 53psl3uzduotis/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int n,u;
+    ifstream fd("data.txt");
+    ofstream fr("rez.txt");
+    fd >> n;
+    s=0;
+    k=0;
+    for(int i=1; i<=n; i++){
+        fd >> u;
+        if(p<0)
+        {
+            k++;
+            s=s+p;
+        }
+    }
+    fr << "prekes ne didesnes uz 14 litu, yra k = "<< k << endl;
+    fr << "Uz jas reikes moketi s = "<< s << endl;
+    fd.close();
+    fr.close();
+    return 0;
+}
+
+
