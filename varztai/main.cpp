ac1605eb52ffcd154eece392565eb7f3a0c13636
MrMixerr <52745857+MrMixerr@users.noreply.github.com>
2025-11-30 17:27:12 +0200

    nu tiipo baigiau bet nevisai

--- varztai/main.cpp
+++ varztai/main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 const int MAX = 100;
 
 using namespace std;
+
 void Skaitymas(int& n, int& m, int A[], int B[]);
 void Reikalinga(int& n, int& m, int A[], int B[]);
+void Pasalinti(int &n, int A[], int B[], int rind);
+void Sudelioti(int n, int A[], int B[]);
 
 int main()
 {
     int n,m;
     int Varz[MAX];
     int Verz[MAX];
+    int Truk_verz[MAX];
+    int Truk_varz[MAX];
+
+
+
     Skaitymas(n,m,Varz,Verz);
     Reikalinga(n,m,Varz,Verz);
+    Reikalinga(n,m,Varz,Verz);
+    Sudelioti(n,Varz,Truk_verz);
 
     return 0;
 }
@@ -33,15 +44,38 @@ void Skaitymas(int& n, int& m, int A[], int B[]){
 }
 
 void Reikalinga(int& n, int& m, int A[], int B[]){
-    int ka=0;
     for(int i=0; i<n; i++){
-        cout << A[i] << endl;
-        for(int j=0; j<m; j++){
+        for(int j=0; j<=m; j++){
             if(A[i]==B[j]){
-                cout << A[i] << " ";
+                Pasalinti(n,A,B,i);
+                Pasalinti(m,B,A,j);
             }
         }
-        cout << endl;
     }
 }
+
+void Pasalinti(int &n, int A[], int B[], int rind){
+    for(int i = rind; i <= n-1; i++){
+        A[i]=A[i+1];
+    }
+    n--;
+}
+
+void Sudelioti(int n, int A[], int B[]){
+    int liek=0;
+    int kiek=0;
+    for(int i=0; i<n; i++){
+        liek=A[i];
+        if(liek==A[i+1]){
+            kiek++;
+            cout << kiek << endl;
+        }
+        else{
+            liek=A[i];
+            cout << liek << endl;
+        }
+
+    }
+}
+
 
